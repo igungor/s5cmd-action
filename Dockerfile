908ee417ab915c3cc870789925fb1f5bfa08b6ee
İbrahim Güngör <igungor@gmail.com>
2021-09-21 11:09:31 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM peakcom/s5cmd:1.3.0
+
+LABEL "com.github.actions.name"="s5cmd"
+LABEL "com.github.actions.description"="Run s5cmd commands"
+LABEL "com.github.actions.icon"="zap"
+LABEL "com.github.actions.color"="yellow"
+
+LABEL version="1.3.0"
+LABEL repository="https://github.com/igungor/s5cmd-action"
+LABEL homepage="github.com/peak/s5cmd"
+LABEL maintainer="Ibrahim Gungor <ibrahim@peak.com>"
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION="us-east-1"
+fi
+
+mkdir /root/.aws
+cat < EOF > /root/.aws/credentials
+[s5cmd-action]
+aws_access_key_id     = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+region = ${AWS_REGION}
+EOF
+
+sh -c "AWS_PROFILE=s5cmd-action s5cmd ${ENDPOINT} $*"
+
+rm -f /root/.aws/credentials
